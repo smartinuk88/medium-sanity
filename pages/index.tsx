d9b0db37d1a0b8bae9755bb0572d9f382a2a8bb8
Scott Martin <scott.martin88@live.com>
2022-01-21 13:41:11 +0100

    Markup and styling for post page, ISR setup to revalidate posts every 60 seconds

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,11 +40,29 @@ export default function Home({ posts }: Props) {
       </div>
 
       {/* Posts */}
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div>
-              <h1>I am a post</h1>
+            <div className="group cursor-pointer border rounded-lg overflow-hidden">
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
             </div>
           </Link>
         ))}
